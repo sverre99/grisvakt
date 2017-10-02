a63e1ed09f1d4cc7f0abbdc6d031f44aea9d05cb
Daniel Svensson <daniel.svensson3@ikea.com>
2017-10-02 17:59:40 +0200

    Updated

--- app.py
+++ app.py
@@ -6,26 +6,26 @@ import cherrypy
 # hack to make sure we can load wsgi.py as a module in this class
 sys.path.insert(0, os.path.dirname(__file__))
 
-virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
-virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
-
-try:
-  #execfile(virtualenv, dict(__file__=virtualenv)) # for Python v2.7
-  #exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # for Python v3.3
-  # Multi-Line for Python v3.3:
-  exec_namespace = dict(__file__=virtualenv)
-  with open(virtualenv, 'rb') as exec_file:
-    file_contents = exec_file.read()
-  compiled_code = compile(file_contents, virtualenv, 'exec')
-  exec(compiled_code, exec_namespace)
-except IOError:
-  pass
-
+# virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+# virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+# 
+# try:
+#   #execfile(virtualenv, dict(__file__=virtualenv)) # for Python v2.7
+#   #exec(compile(open(virtualenv, 'rb').read(), virtualenv, 'exec'), dict(__file__=virtualenv)) # for Python v3.3
+#   # Multi-Line for Python v3.3:
+#   exec_namespace = dict(__file__=virtualenv)
+#   with open(virtualenv, 'rb') as exec_file:
+#     file_contents = exec_file.read()
+#   compiled_code = compile(file_contents, virtualenv, 'exec')
+#   exec(compiled_code, exec_namespace)
+# except IOError:
+#   pass
+# 
 
 # Get the environment information we need to start the server
-ip = os.environ['OPENSHIFT_PYTHON_IP']
-port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
-host_name = os.environ['OPENSHIFT_GEAR_DNS']
+ip = '0.0.0.0'
+port = int(os.environ['WEBAPP_SERVICE_PORT'])
+host_name = os.environ['WEBAPP_SERVICE_HOST']
 environ = os.environ
 
 ### My app
@@ -36,7 +36,7 @@ from time import gmtime, strftime, localtime
 from model_helpers import get_images, download_email, images_by_hour
 from view_index import HTML_HEADER, HTML_FOOTER, html_navbar
 
-myDir = environ['OPENSHIFT_REPO_DIR']
+myDir = environ['HOME']
 
 class GrisVakt(object):
 
